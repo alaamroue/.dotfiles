952b9e9a43deb8bf463b53f2b6d8b9dbe0a92c2d
Alaa Mroue <alaa.f.mroweh@gmail.com>
2024-11-19 22:12:09 +0100

    yml3

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cd ~ && mkdir tempAlaaSetup && cd tempAlaaSetup
 
 # Update and install all
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y libevent-dev bison byacc autotools-dev automake build-essential pkg-config autoconf-archive ncurses-dev tree neovim fzf golang-go xsel glow
 source ~/.bashrc
 
