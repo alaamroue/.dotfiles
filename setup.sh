dbdabd272d604c147baa844470d5a04546a42678
Alaa Mroue <alaa.f.mroweh@gmail.com>
2024-11-19 22:07:01 +0100

    tml

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cd ~ && mkdir tempAlaaSetup && cd tempAlaaSetup
 
 # Update and install all
 sudo apt update
-sudo apt install -y libevent-dev bison byacc autotools-dev automake build-essential pkg-config autoconf-archive ncurses-dev tree neovim fzf golang-go xsel glow
+sudo apt-get install -y libevent-dev bison byacc autotools-dev automake build-essential pkg-config autoconf-archive ncurses-dev tree neovim fzf golang-go xsel glow
 
 # Tmux
 sudo apt remove -y tmux
